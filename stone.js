e5e4f1b92a030db7b563ca20a6ded0409f822a37
KashishB16 <77157229+KashishB16@users.noreply.github.com>
2021-05-08 19:48:08 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,32 @@
+class Stone{
+	constructor(x,y,width,height)
+	{
+		var options = {
+			'density':12,
+			'friction': 0.9,
+			'restitution':0.8
+		  };
+	// assign options to the rubber ball
+		
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+            var pos=this.body.position;	
+            var angle=this.body.angle;
+			push()
+			translate(pos.x, pos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+			//draw the ellipse here to display the rubber ball
+            rect(0,0,this.width,this.height)
+			pop()
+	}
+
+}
